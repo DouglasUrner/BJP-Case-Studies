a90fe4f31c853b3e6928640ab970851aceaf5abc
Douglas Urner <dlu@canishe.com>
2016-10-02 21:33:17 -0700

    Update class comment.

---
+++ src/DrawFigures1.java
@@ -0,0 +1,43 @@
+/**
+ * DrawFigures1 - Starting point for BJP Case Study 1.5: DrawFigures.
+ *
+ * Created by dlu on 02.10.2016.
+ */
+public class DrawFigures1 {
+    public static void main(String[] args) {
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+
+        System.out.println("");
+
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+
+        System.out.println("");
+
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("|United|");
+        System.out.println("|States|");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+    }
+}
